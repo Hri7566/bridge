92d0fd536cdeee473add04ae4bd0213c8eeedd27
Hri7566 <hri7566@gmail.com>
2023-01-10 03:49:44 -0500

    thing

--- src/ChannelManager/ChannelManager.ts
+++ src/ChannelManager/ChannelManager.ts
@@ -4,6 +4,11 @@ const defaultConfig: ChannelManagerConfig = {
 
 export class ChannelManager {
 	public static start(config: ChannelManagerConfig = defaultConfig): void {
-		
+		for (let bridge of config.bridges) {
+			for (let ch of bridge.channels) {
+				// each channel has its own connection rules based on ch.type
+				
+			}
+		}
 	}
 }
